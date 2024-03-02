52822e9410039452d4860d307896f1a0e84eda76
PiotrDabrowski232 <piotr.dabrowski2001@o2.pl>
2024-03-02 14:14:38 +0100

    some changes

--- CarTroubleSolver/CarTroubleSolver.Data/obj/Release/net8.0/CarTroubleSolver.Data.AssemblyInfo.cs
+++ CarTroubleSolver/CarTroubleSolver.Data/obj/Release/net8.0/CarTroubleSolver.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarTroubleSolver.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+243468f016d25e72f12068042aafad136b5e2919")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2a9ffb5aa2f3c6f3cf2d3a626817b102a994f26")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarTroubleSolver.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarTroubleSolver.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
