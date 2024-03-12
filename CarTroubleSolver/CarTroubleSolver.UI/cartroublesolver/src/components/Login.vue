fadf7a319a82debc55f68bafab8ae2f224e2fb98
PiotrDabrowski232 <piotr.dabrowski2001@o2.pl>
2024-03-12 22:37:47 +0100

    first steps with frontend

---
+++ CarTroubleSolver/CarTroubleSolver.UI/cartroublesolver/src/components/Login.vue
@@ -0,0 +1,83 @@
+<template>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">Navbar</a>
+    
+      <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Login</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Register</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    
+    
+      <div class="main">
+        <h1>Login Vue</h1>
+      </div>
+    
+    
+    
+      <div class="footer">
+    
+      </div>
+    
+    
+    </template>
+    
+    <script>
+    
+    export default {
+      name: 'Login'
+    }
+    </script>
+    
+    <style>
+    
+    body{
+      margin:auto;
+    }
+    
+    #app {
+      font-family: Avenir, Helvetica, Arial, sans-serif;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      text-align: center;
+      color: #2c3e50;
+    }
+    nav{
+      width: 100%;
+      height: 8vh;
+      border-bottom: 1px solid black;
+    }
+    
+    .navbar-brand{
+      padding-left: 2.8vw;
+      margin-top: 1vw;
+    }
+    
+    #navbarNav{
+      margin-right: 4vw;
+    }
+    
+    .navbar-nav{
+      margin-top: 1.2vh;
+      padding-right: 2vw;
+    }
+    
+    .main{
+      width: 100%;
+      height: 87vh;
+    }
+    
+    .footer{
+      background-color: rgb(0, 0, 0);
+      width: 100%;
+      height: 5vh;
+    }
+    </style>
+    
--- CarTroubleSolver/CarTroubleSolver.UI/cartroublesolver/src/main.js
+++ CarTroubleSolver/CarTroubleSolver.UI/cartroublesolver/src/main.js
@@ -2,11 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import PrimeVue from 'primevue/config';
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 import 'primevue/resources/themes/aura-light-green/theme.css'
 
 
 import Button from "primevue/button"
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 const app = createApp(App);
 app.use(PrimeVue);
